0288312c5200ccd1339b7ea806572d2d272eab9f
Michael <Michael.leenj1999@gmail.com>
2022-03-04 15:18:53 +1100

    Tidy Up - Added selectors.json

--- juicer.js
+++ juicer.js
@@ -3,20 +3,9 @@ const fs = require('fs/promises');
 const { IncomingMessage } = require('http');
 const {GoogleSpreadsheet} = require('google-spreadsheet');
 const creds = require('./client-secret.json')
-const testListing = '#post-23622 > div > div > div > div > div > div > ul > li > a > div > h3'
-const paginationSelector = "#post-23622 > div > div > div > div > div > div.job_listings > nav > ul > li"
-const formSelector = "#search_categories";
-const checkForm = '//*[@id="post-23622"]/div/div[2]/div/div/div/div[3]/form/div[1]/div[4]/span/span[1]/span/ul/li/input'
-const resultsFoundSelector = '#post-23622 > div > div.fusion-fullwidth.fullwidth-box.fusion-builder-row-3.nonhundred-percent-fullwidth.non-hundred-percent-height-scrolling > div > div > div > div.job_listings > form > div.showing_jobs.wp-job-manager-showing-all > span';
-const listing = '#post-23622 > div > div.fusion-fullwidth.fullwidth-box.fusion-builder-row-3.nonhundred-percent-fullwidth.non-hundred-percent-height-scrolling > div > div > div > div.job_listings > ul > li.post-68426.job_listing.type-job_listing.status-publish.hentry.job_listing_tag-deakintalent-jobs-board.job-residency-not-specified.job-type-internships > a > div.position > h3'
-const volunteerCheckbox = '#job_type_volunteering'
-const jobsCheckbox = '#job_type_jobs'
-const lastChild = "#post-23622 > div > div.fusion-fullwidth.fullwidth-box.fusion-builder-row-3.nonhundred-percent-fullwidth.non-hundred-percent-height-scrolling > div > div > div > div.job_listings > nav > ul > li:last-child"
-const testJuice = "#post-23622 > div > div.fusion-fullwidth.fullwidth-box.fusion-builder-row-3.nonhundred-percent-fullwidth.non-hundred-percent-height-scrolling > div > div > div > div.job_listings > nav > ul > li:nth-child(2) > a"
-//Need to clean this up, place in json?? And Trim the Selectors
+const selector = require('./selectors.json')
 
 const doc = new GoogleSpreadsheet('1ZRfUEe3PxUVPoyz7nZByH4y1xKjGQjq3UFXDh-twKj8'); //Initializing Sheet
-
 
 async function start() {
     const browser = await puppeteer.launch()
@@ -28,12 +17,10 @@ async function start() {
     })
     console.log(pageHeading);
 
-    //Text can input and then press the enter key to submit, just entering the text doesn't work.
-
-    await formInput(formSelector, page)
-    await tickCheckbox(volunteerCheckbox, jobsCheckbox, page)
-    await resultsFound(resultsFoundSelector, page)
-    await findListings(testListing, lastChild, page)
+    await formInput(selector.formSelector, page)
+    await tickCheckbox(selector.volunteerCheckbox, selector.jobsCheckbox, page)
+    await resultsFound(selector.resultsFoundSelector, page)
+    await findListings(selector.listing, selector.pagination, page)
 
     await browser.close()
 }
@@ -69,7 +56,7 @@ async function findListings(selector, paginationSelector, page) {
         }
         else {
             console.log(paginationNew);
-            await page.click(lastChild)
+            await page.click(paginationSelector)
             await page.waitForSelector(selector, {visible: true})
         }     
     }
@@ -95,6 +82,13 @@ async function AccessSpreadsheet(ListingsArray)
     });
     await doc.loadInfo()
     const sheet = doc.sheetsByIndex[0] // Which sheet we are actually using
+
+    const rows = await sheet.getRows();
+    for(let i = 0; i <= ListingsArray.length; i++)
+    {
+        const rows = await sheet.addRow({Title: ListingsArray[i]})
+    }
+
     //Nope dumb
     // ListingsArray.forEach(listing => {
     //     listing = formattedArray.title;
@@ -105,13 +99,6 @@ async function AccessSpreadsheet(ListingsArray)
     // ListingsArray.forEach(async listing => {
     //     const thisRow = await sheet.addRow({Title: listing});
     // })
-    const rows = await sheet.getRows();
-    for(let i = 0; i <= ListingsArray.length; i++)
-    {
-        const rows = await sheet.addRow({Title: ListingsArray[i]})
-    }
-
-    
 
 }
 
